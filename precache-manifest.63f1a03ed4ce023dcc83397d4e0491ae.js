10de2da03079926eddf7c8a4738277714ae20bfd
Yanay Zabary <yanay.tsabary@gmail.com>
2020-07-20 17:08:55 +0300

    Updates

---
+++ precache-manifest.63f1a03ed4ce023dcc83397d4e0491ae.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d39f034e124206be13333859ee77137",
+    "url": "/logic-games/index.html"
+  },
+  {
+    "revision": "6d676aeda30286a3ece5",
+    "url": "/logic-games/static/css/main.422a8fc6.chunk.css"
+  },
+  {
+    "revision": "e3bdd2d8f5f7aec4b7ad",
+    "url": "/logic-games/static/js/2.c20b1afe.chunk.js"
+  },
+  {
+    "revision": "60a46cfe2bfe9b85be1a545484ea64d1",
+    "url": "/logic-games/static/js/2.c20b1afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d676aeda30286a3ece5",
+    "url": "/logic-games/static/js/main.1c0767d9.chunk.js"
+  },
+  {
+    "revision": "74c8409556bf436a2add",
+    "url": "/logic-games/static/js/runtime-main.a98f3e0b.js"
+  },
+  {
+    "revision": "b006268b9b53a8ada84c1caf0cf79a42",
+    "url": "/logic-games/static/media/agree.b006268b.mp3"
+  },
+  {
+    "revision": "5ca76dd76e69c85cae8fbe9177399b55",
+    "url": "/logic-games/static/media/castle.5ca76dd7.svg"
+  },
+  {
+    "revision": "c0e4ffa80114edc2939699d5c43d0389",
+    "url": "/logic-games/static/media/check.c0e4ffa8.svg"
+  },
+  {
+    "revision": "1ebb571687d19139a3a2c0d55f5ce811",
+    "url": "/logic-games/static/media/chimney.1ebb5716.svg"
+  },
+  {
+    "revision": "745cbb6591eee968962a7b6d0b15d01c",
+    "url": "/logic-games/static/media/click.745cbb65.mp3"
+  },
+  {
+    "revision": "c03325cbee6aa9ecce96471370454d17",
+    "url": "/logic-games/static/media/correct.c03325cb.wav"
+  },
+  {
+    "revision": "437c5d744b753313cec573a53cadd229",
+    "url": "/logic-games/static/media/countdownFinal.437c5d74.wav"
+  },
+  {
+    "revision": "04e72d2ffb4f3d3345d1c279a77bee0c",
+    "url": "/logic-games/static/media/countdownGo.04e72d2f.wav"
+  },
+  {
+    "revision": "df987d19177d2bb05943c9fc40e92804",
+    "url": "/logic-games/static/media/countdownStart.df987d19.wav"
+  },
+  {
+    "revision": "c56ee88340b78028131b5dc0bcceeab4",
+    "url": "/logic-games/static/media/finished.c56ee883.wav"
+  },
+  {
+    "revision": "8a657a5904e774879a66260ae7672004",
+    "url": "/logic-games/static/media/hammer.8a657a59.svg"
+  },
+  {
+    "revision": "39847edfed1c7d7651579f68712e258e",
+    "url": "/logic-games/static/media/heart.39847edf.svg"
+  },
+  {
+    "revision": "b214e31ba49423730a8f5840242b6bf1",
+    "url": "/logic-games/static/media/heart_half.b214e31b.svg"
+  },
+  {
+    "revision": "ca43bf4e448fd9e7fac68d6b5966d181",
+    "url": "/logic-games/static/media/heart_lost.ca43bf4e.svg"
+  },
+  {
+    "revision": "ba8ecde5990dfb20916a1503c6b1095e",
+    "url": "/logic-games/static/media/hier1.ba8ecde5.svg"
+  },
+  {
+    "revision": "db5b0f721954c235cd66a0a2d5c8bb2b",
+    "url": "/logic-games/static/media/hier10.db5b0f72.svg"
+  },
+  {
+    "revision": "3a6e4beb199ebe7ab67399bd13525192",
+    "url": "/logic-games/static/media/hier11.3a6e4beb.svg"
+  },
+  {
+    "revision": "1358995e7f274b76f9a66ea961ab0eea",
+    "url": "/logic-games/static/media/hier12.1358995e.svg"
+  },
+  {
+    "revision": "85fbe755a9ea06add6027444770e04ce",
+    "url": "/logic-games/static/media/hier13.85fbe755.svg"
+  },
+  {
+    "revision": "18c4c23d263be0a797f3d397f4f885e9",
+    "url": "/logic-games/static/media/hier14.18c4c23d.svg"
+  },
+  {
+    "revision": "3c1aebc1d7c29fc70cbe54cff828559c",
+    "url": "/logic-games/static/media/hier2.3c1aebc1.svg"
+  },
+  {
+    "revision": "73e87f49cf7d5f04e307128ab3153e73",
+    "url": "/logic-games/static/media/hier3.73e87f49.svg"
+  },
+  {
+    "revision": "f478b2138967cfc6932b24c9e92ade12",
+    "url": "/logic-games/static/media/hier4.f478b213.svg"
+  },
+  {
+    "revision": "7670c97532759a72b2db8995d103151d",
+    "url": "/logic-games/static/media/hier5.7670c975.svg"
+  },
+  {
+    "revision": "71c3e61b83ca7d8b5a81af3c417b252e",
+    "url": "/logic-games/static/media/hier6.71c3e61b.svg"
+  },
+  {
+    "revision": "ad03f615a09f64f2e04d974709bbab7d",
+    "url": "/logic-games/static/media/hier7.ad03f615.svg"
+  },
+  {
+    "revision": "076dc4e490fd26bc3fc730e27c3d7187",
+    "url": "/logic-games/static/media/hier8.076dc4e4.svg"
+  },
+  {
+    "revision": "b45de614476441cc9f3735f0a1ff3ce1",
+    "url": "/logic-games/static/media/hier9.b45de614.svg"
+  },
+  {
+    "revision": "5abcc96ee0ef1c985ae2a829ecd805fe",
+    "url": "/logic-games/static/media/horse.5abcc96e.svg"
+  },
+  {
+    "revision": "990a625cf738ce71f46ab04250e42123",
+    "url": "/logic-games/static/media/wrong.990a625c.wav"
+  },
+  {
+    "revision": "62eefd11215424ec653ab194330af840",
+    "url": "/logic-games/static/media/x.62eefd11.svg"
+  }
+]);
