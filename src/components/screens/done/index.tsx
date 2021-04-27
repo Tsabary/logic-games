b33c4ad60e3cdf1720b22ac1f856ceb3a4c715c5
Yanay Zabary <yanay.tsabary@gmail.com>
2021-04-27 09:33:31 +0300

    modified the done screen to be more customized

--- src/components/screens/done/index.tsx
+++ src/components/screens/done/index.tsx
@@ -4,15 +4,31 @@ import { gameInfoContext } from "../../../providers/GameInfo";
 import strings from "../../../constants/localizedStrings";
 
 const Done = () => {
-  const { isDone, score } = useContext(gameInfoContext);
+  const { isDone, accuracy, score, challenge } = useContext(gameInfoContext);
+
+  const doneMessaage = () => {
+    switch (challenge) {
+      // For the typing test we have a different message
+      case 5:
+        return (
+          <div>
+            {`${score} ${strings.wpm}`}
+            <br />
+            {`${accuracy}% ${strings.accuracy}`}{" "}
+          </div>
+        );
+
+      // The basic "Your score is" message
+      default:
+        return `${strings.youScored} ${score}`;
+    }
+  };
 
   return (
     <div className="done" style={{ display: isDone ? "block" : "none" }}>
       <div className="done__container">
         <h1>{strings.thankYou}</h1>
-        <div className="done__score">
-          {strings.youScored} {score}
-        </div>
+        <div className="done__score">{doneMessaage()}</div>
         <h2 style={{ marginTop: "4rem" }}>{strings.thankYouMessage}</h2>
       </div>
     </div>
